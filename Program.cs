ff2294df77620528e6ebe9faad638eea87033d6c
DevHobbyist <l3alazskiss15@gmail.com>
2022-09-20 08:58:12 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables!");
+            _ = Console.ReadKey(true);
+
+            // this is a comment
+
+            // variables
+        }
+    }
+}
