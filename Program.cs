bc4356fdabb564f51ff59fffcedb6db46270862d
DevHobbyist <l3alazskiss15@gmail.com>
2022-09-20 09:39:16 -0300

    Added variables inside script

--- Program.cs
+++ Program.cs
@@ -11,11 +11,34 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("Variables!");
-            _ = Console.ReadKey(true);
-
-            // this is a comment
 
             // variables
+
+            // integers (whole numbers)
+            int score; // declaration -- declare "score" is an integer
+            score = 0; // initialization -- init. "score" to be 0.
+            int enemyPoints;
+            enemyPoints = 50; // 50 pts per kill
+            //score = 1000000;
+            score = score + enemyPoints; // enemy killed
+            Console.WriteLine("Score: " + score); // display score
+            Console.WriteLine(score); // display score
+
+            // floating points
+            float lapTime; // declaration
+            lapTime = 0.0f;
+            Console.WriteLine(lapTime); // display score
+            lapTime = 65.357f;
+            Console.WriteLine(lapTime); // display score
+
+            // string (text)
+            string title; // declaration
+            title = "Newbie";
+            Console.WriteLine(title);
+            title = "Pro";
+            Console.WriteLine(title);
+
+            Console.ReadKey(true); // wait for keypress
         }
     }
 }
